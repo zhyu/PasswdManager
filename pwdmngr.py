a49cbb1c78fb7701c5d60628f2f9a666d0947a3c
Zhang Yu <angellwings@gmail.com>
2012-05-17 21:29:09 +0800

    pwdmngr.py added. not complete yet.

---
+++ pwdmngr.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+import sys, config
+from PyQt4 import QtGui
+from gui.dialogs import LoginDialog
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.resize(200, 200)
+        
+def login():
+    loginDialog = LoginDialog()
+    loginDialog.exec_()
+    return loginDialog.authentication()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    win = MainWindow()
+    if login(): 
+        win.show()
+        sys.exit(app.exec_())
+    
+if __name__ == '__main__':
+    main()
