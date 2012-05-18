1a5c40b66745adf0bd9ba8d7d1b91bc9dc5bdde1
Zhang Yu <angellwings@gmail.com>
2012-05-18 14:38:55 +0800

    some dialog updated.

--- gui/myGui.py
+++ gui/myGui.py
@@ -1,7 +1,74 @@
 # -*- coding:utf-8 -*-
 
 import config, dialogs
-from PyQt4 import QtGui
+from PyQt4.QtGui import QMessageBox
+
+##################################
+# GUI size/position properties
+##################################
+#Application window size,position
+MAIN_WINDOW_SIZE=(1100,700)
+MAIN_WINDOW_POSITION=(100,80)
+#Dialog position is needed for windows platform.
+DIALOG_POSITION=(270,150)
+
+# Long Textbox
+SIZE_LONG_TEXT=(300,28)
+SIZE_NORMAL_TEXT=(200,28)
+SIZE_MULTILINE_TEXT=(400,100)
+
+# textbox in create/edit/detail dialog. keeping same length with multilineTextBox
+SIZE_DETAIL_TEXT= (400,28) 
+SIZE_SECRET_TEXT= (400,200) 
+
+# Dialog
+SIZE_DIALOG_LOGIN = (400, 130)
+SIZE_DIALOG_NEWACCOUNT = (900, 610)
+SIZE_DIALOG_EDITACCOUNT = (900,610)
+SIZE_DIALOG_ACCOUNTDETAIL = (650,680)
+SIZE_DIALOG_CHGMASTERPWD = (500, 240)   
+SIZE_DIALOG_PWDGEN = (520, 360)      
+
+# splitterwindow min pane size
+SPLITTERWINDOW_MIN_SIZE = 100 
+
+##################################
+# GUI Position properties
+##################################
+
+
+# splitterwindow Sash Position
+SPLITTERWINDOW_SASH_POS = 200 
+
+
+##################################
+# GUI Window name
+##################################
+TAG_LIST_NAME = 'tagListCtrl'
+PWD_LIST_NAME = 'pwdListCtrl'
+NAME_TEXTBOX_SEARCH = 'searchTextBox'
+
+##################################
+#Information messages
+##################################
+INFO_CLIPBOARD = """ The password of [%s] has been copied to Clipboard. """
+INFO_PWD_CLIPBOARD = """ The password has been copied to Clipboard. """
+INFO_MOVETO_TRASH = """ The account [%s] has been moved to trash,\n you can recover at any time """
+INFO_COMPLETE_REMOVE = """ The account [%s] has been deleted from trash. """
+INFO_RECOVERED = """ The account [%s] has been recovered from trash. """
+INFO_MASTERPWD = """ The Master password was successfully changed. """
+INFO_HIDE_TXT = """[ Hidden ]"""
+INFO_ACCOUNT_ACTIVE = """[ Active ]"""
+INFO_ACCOUNT_INTRASH = """[ In Trash ]"""
+
+##################################
+#Confirmation messages
+##################################
+CONFIRM_MOVETO_TRASH = """Are you sure to move the account [%s]?"""
+CONFIRM_COMPLETE_REMOVE = """Are you sure to PERMANENTLY DELETE the account [%s]?"""
+CONFIRM_EMPTY_TRASH = """Are you sure to empty the trash?"""
+CONFIRM_REMOVE_TAG = """Are you sure to remove the tag [%s]?"""
+CONFIRM_REMOVE_USEDTAG = """Removing tag [%s] will NOT remove related accounts. Continue to remove Tag?"""
 
 ##################################
 #Error messages
@@ -22,5 +89,20 @@ ERR_PWD_EMPTYPATTERN= 'At least one Password Pattern needs to be choosen!'
 ERR_PWD_COPY= 'Before do the coping, please generating a password first.'
 
 def showErrorDialog(errMsg):
-    dlg = dialogs.MsgDialog(None, 'Error', errMsg)
-    dlg.exec_()
+    QMessageBox.critical(None, 'Error', errMsg, QMessageBox.Ok)
+    
+def showConfirmationDialog(msg, name=''):
+    if not name:
+        return QMessageBox.question(None, 'Confirmation', msg, QMessageBox.Cancel | QMessageBox.Ok, QMessageBox.Cancel)
+    else:
+        return QMessageBox.question(None, 'Confirmation', msg % name, QMessageBox.Cancel | QMessageBox.Ok, QMessageBox.Cancel)
+
+def showInfoDialog(msg, name=''):
+    if not name:
+        QMessageBox.information(None, 'Infomation', msg, QMessageBox.Ok)
+    else:
+        QMessageBox.information(None, 'Infomation', msg % name, QMessageBox.Ok)
+    
+def showAboutDialog():
+    description = """<h1><font size=500>Password Manager</font></h1>\n is a simple password management tool written in python and qt """
+    QMessageBox.about(None, 'About', description)
