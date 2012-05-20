ad2acdf0bef10fd4fa24bc2dc5121daa92d7b6d5
Zhang Yu <angellwings@gmail.com>
2012-05-20 20:41:38 +0800

    no dist.

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-import glob, py2exe
-
-setup(windows=[{"script":"pwdmngr.py","icon_resources":[(1,"gui/icons/app.ico")]}], 
-      data_files=[("data",glob.glob("data/*.*")),
-                  ("icons",glob.glob("gui/icons/*.png"))] 
-      )
